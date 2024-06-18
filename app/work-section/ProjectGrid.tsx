3adbe0d78cce5b432ae0ccdafa8aa44e3fbf2499
Abhi Lokhande <AbhijeetLokhande2580@gmail.com>
2024-06-18 12:44:56 +0530

    ldcn

--- app/work-section/ProjectGrid.tsx
+++ app/work-section/ProjectGrid.tsx
@@ -1,5 +1,5 @@
 import ProjectCard from "./ProjectCard";
-import { devProjects, designProjects, ProjectProps } from "./projectDetails";
+import { devProjects, /*designProjects ,*/ ProjectProps } from "./projectDetails";
 import { useState } from "react";
 
 const ProjectGrid = () => {
@@ -7,26 +7,6 @@ const ProjectGrid = () => {
 
   return (
     <>
-      {/* <div className="mb-10 flex gap-16 text-[#e4ded7] md:mb-16  lg:mb-20 ">
-        <h4
-          className={`text-[16px] md:text-[20px] lg:text-[24px] ${
-            filter ? "text-[#e4ded7]" : "text-[#e4ded7]/30"
-          }`}
-          onClick={() => setFilter(true)}
-        >
-          Development
-        </h4>{" "}
-        <h4
-          className={`text-[16px] md:text-[20px] lg:text-[24px] ${
-            filter ? "text-[#e4ded7]/30" : "text-[#e4ded7]"
-          }`}
-          onClick={() => setFilter(false)}
-        >
-          Design
-        </h4>
-      </div> */}
-
-      {/* {filter ? ( */}
         <div className="grid w-[90%] grid-cols-1 grid-rows-2 gap-y-10 gap-x-6 lg:max-w-[1200px] lg:grid-cols-1">
           {devProjects.map((project: ProjectProps) => (
             <ProjectCard
@@ -42,24 +22,6 @@ const ProjectGrid = () => {
             />
           ))}
         </div>
-      {/* ) : (
-        <div className="grid w-[90%] grid-cols-1 grid-rows-2 gap-y-6 gap-x-6 lg:max-w-[1200px] lg:grid-cols-1">
-          {designProjects.map((project: ProjectProps) => (
-            <ProjectCard
-              id={project.id}
-              key={project.id}
-              name={project.name}
-              description={project.description}
-              technologies={project.technologies}
-              github={project.github}
-              demo={project.demo}
-              image={project.image}
-              bgColor={project.bgColor}
-              available={project.available}
-            />
-          ))}
-        </div>
-      )} */}
     </>
   );
 };
--- app/work-section/projectDetails.ts
+++ app/work-section/projectDetails.ts
@@ -12,128 +12,95 @@ export type ProjectProps = {
 export const devProjects = [
   {
     id: 0,
-    name: "Propellent",
+    name: "Open Throttle's",
     description:
-      "A Landing Page built for software startups and small businesses, to showcase their services and mark their online presence.",
-    technologies: ["React", "Tailwind CSS", "Framer Motion"],
-    github: "https://github.com/victorcodess/propellent",
-    demo: "https://propellent.vercel.app/",
-    image: require(".//../../public/projects/propellent-new-flip.png"),
+      "Experience seamless bike browsing, detailed model insights, and effortless showroom connections",
+    technologies: ["MERN","Bootstarp"],
+    github: "https://github.com/Abhilokhande/Open-Throttle",
+    demo: " ",
+    image: require(".//../../public/projects/open-throttle.png"),
     available: true,
   },
   {
     id: 1,
-    name: "Flixify",
+    name: "MoneyMate",
     description:
-      "Flixify lets you seamlessly explore movies and TV series, add bookmarks, and search across all pages. It offers user authentication along with a theme switch.",
-    technologies: ["Next.js", "Typescript", "Prisma"],
-    github: "https://github.com/victorcodess/flixify",
-    demo: "https://flixify.victorwilliams.me/",
+      "Your ultimate financial companion, seamlessly blending technology and expertise for effortless financial management.Experience the future of finance today!",
+    technologies: ["JAVA(JSp)", "HTML", "CSS"],
+    github: "https://github.com/Abhilokhande/Online-Banking",
+    demo: "",
     image: require(".//../../public/projects/flixify-flip.png"),
     available: true,
   },
   {
     id: 2,
-    name: "SkyWatch",
+    name: "RhythmicNodes",
     description:
-      "SkyWatch is a convenient and user-friendly tool that allows you to quickly and easily check the current weather and forecast for the next 2 days in any city.",
-    technologies: ["React", "CSS", "Chart.js"],
-    github: "https://github.com/victorcodess/weather-forecast-website",
-    demo: "https://sky-watch.vercel.app/",
+     "RhythmicNodes: A console music player demonstrating linked list power. Weaving melodies through nodes, it offers dynamic playback, blending tech and music seamlessly.",
+    technologies: ["C++", "Data Structure"],
+    github: "https://github.com/Abhilokhande/DCLL-music-player....",
+    demo: "",
     image: require(".//../../public/projects/skywatch-flip.png"),
     available: true,
   },
   {
     id: 3,
-    name: "Alpaca Image Generator",
+    name: "PathPuzzle",
     description:
-      "An image generator website that allows users to generate, combine, and download images.",
-    technologies: ["React", "CSS", "Merge Images"],
-    github: "https://github.com/victorcodess/alpaca-image-generator",
-    demo: "http://alpaca-image-generator-beta.vercel.app",
+     "PathPuzzle: A MERN stack project utilizing the A* algorithm to find the shortest path. With its CSS-crafted block design, it offers an immersive gaming experience. Challenge yourself to navigate through mazes and puzzles, blending entertainment with algorithmic complexity.",
+    technologies: ["React", "CSS", "Node.Js"],
+    github: "https://github.com/Abhilokhande/Case-Study",
+    demo: "2",
     image: require(".//../../public/projects/alpaca-flip.png"),
     available: true,
   },
-
-  // {
-  //   id: 3,
-  //   name: "Link Shortener",
-  //   description:
-  //     "A website that reduces the length of your URL using Bit.ly's API",
-  //   technologies: ["JavaScript", "CSS", "Bit.ly's API"],
-  //   github: "https://github.com/victorcodess/url-shortener",
-  //   demo: "https://url-shortener-nine-delta.vercel.app",
-  //   image: require(".//../../public/projects/shortener-new.webp"),
-  //   available: true,
-  // },
-  // {
-  //   id: 4,
-  //   name: "Carpooling Service",
-  //   description:
-  //     "TMTM helps Covenant University students find fellow students who are headed to the same location, so they can share a ride and split the cost.",
-  //   technologies: ["Material UI", "React", "Formik"],
-  //   github: "https://github.com/victorcodess/carpooling-service",
-  //   demo: "",
-  //   image: require(".//../../public/projects/carpool-new.webp"),
-  //   available: false,
-  // },
-  // {
-  //   id: 5,
-  //   name: "MLSC.ng",
-  //   description:
-  //     "This is platform for Microsoft Learn Student Ambassadors to shorten links, append their sharing IDs and generate event certificates.",
-  //   technologies: ["Next.js", "Next Auth", "Tailwind CSS"],
-  //   github: "https://github.com/msp-nigeria/mlsc.ng-frontend",
-  //   demo: "",
-  //   image: require(".//../../public/projects/mlsc.png"),
-  //   available: false,
-  // },
 ];
 
-export const designProjects = [
-  {
-    id: 1,
-    name: "Hebron Statup Lab Website",
-    description:
-      "SkyWatch is a convenient and user-friendly tool that allows you to quickly and easily check the current.",
-    technologies: ["UX Research", "UI Design", "Prototyping"],
-    github: "",
-    demo: "",
-    image: "/_next/image?url=%2F..%2Fpublic%2Fprojects%2Fhsl.webp&w=1920&q=75",
-    available: false,
-  },
-  {
-    id: 2,
-    name: "RAGS Scrubs Website",
-    description:
-      "An image generator website that allows users to generate, combine, and download images.",
-    technologies: ["UX Research", "UI Design", "Prototyping"],
-    github: "",
-    demo: "",
-    image: "/_next/image?url=%2F..%2Fpublic%2Fprojects%2Frags.webp&w=1920&q=75",
-    available: false,
-  },
-  {
-    id: 3,
-    name: "Crown Branding Agency Website",
-    description:
-      "A website that reduces the length of your URL using Bit.ly's API",
-    technologies: ["UX Research", "UI Design", "Prototyping"],
-    github: "",
-    demo: "",
-    image:
-      "/_next/image?url=%2F..%2Fpublic%2Fprojects%2Fcrown.webp&w=1920&q=75",
-    available: false,
-  },
-  {
-    id: 4,
-    name: "Titi Mobile App",
-    description:
-      "TMTM helps you find people who are headed to the same location as you, so you can share a ride and split the cost with them.",
-    technologies: ["UX Research", "UI Design", "Prototyping"],
-    github: "",
-    demo: "",
-    image: "/_next/image?url=%2F..%2Fpublic%2Fprojects%2Ftiti.webp&w=1920&q=75",
-    available: false,
-  },
-];
+// export const designProjects = [
+//   {
+//     id: 1,
+//     name: "Hebron Statup Lab Website",
+//     description:
+//       "SkyWatch is a convenient and user-friendly tool that allows you to quickly and easily check the current.",
+//     technologies: ["UX Research", "UI Design", "Prototyping"],
+//     github: "",
+//     demo: "",
+//     image: "/_next/image?url=%2F..%2Fpublic%2Fprojects%2Fhsl.webp&w=1920&q=75",
+//     available: false,
+//   },
+//   {
+//     id: 2,
+//     name: "RAGS Scrubs Website",
+//     description:
+//       "An image generator website that allows users to generate, combine, and download images.",
+//     technologies: ["UX Research", "UI Design", "Prototyping"],
+//     github: "",
+//     demo: "",
+//     image: "/_next/image?url=%2F..%2Fpublic%2Fprojects%2Frags.webp&w=1920&q=75",
+//     available: false,
+    
+//   },
+//   {
+//     id: 3,
+//     name: "Crown Branding Agency Website",
+//     description:
+//       "A website that reduces the length of your URL using Bit.ly's API",
+//     technologies: ["UX Research", "UI Design", "Prototyping"],
+//     github: "",
+//     demo: "",
+//     image:
+//       "/_next/image?url=%2F..%2Fpublic%2Fprojects%2Fcrown.webp&w=1920&q=75",
+//     available: false,
+//   },
+//   {
+//     id: 4,
+//     name: "Titi Mobile App",
+//     description:
+//       "TMTM helps you find people who are headed to the same location as you, so you can share a ride and split the cost with them.",
+//     technologies: ["UX Research", "UI Design", "Prototyping"],
+//     github: "",
+//     demo: "",
+//     image: "/_next/image?url=%2F..%2Fpublic%2Fprojects%2Ftiti.webp&w=1920&q=75",
+//     available: false,
+//   },
+// ];
