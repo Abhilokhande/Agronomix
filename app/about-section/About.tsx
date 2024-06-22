462d877e32bf30b72caf015238319107f42d79c9
Abhi Lokhande <AbhijeetLokhande2580@gmail.com>
2024-06-22 16:12:20 +0530

    will complete today

--- app/about-section/About.tsx
+++ app/about-section/About.tsx
@@ -1,4 +1,4 @@
-import SongCarousel from "./SongCarousel";
+// import SongCarousel from "./SongCarousel";
 import "../animations/animate.css";
 import AnimatedBody from "../animations/AnimatedBody";
 import AnimatedTitle from "../animations/AnimatedTitle";
@@ -99,14 +99,14 @@ const About = () => {
             </div>
           </div>
         </div>
-        <div className="mt-10 flex flex-col md:-mt-0 lg:mt-28">
-          <SongCarousel />
-          <AnimatedBody
+        {/* <div className="mt-10 flex flex-col md:-mt-0 lg:mt-28"> */}
+          {/* <SongCarousel /> */}
+          {/* <AnimatedBody
             text="A few songs I can recommend if you're looking for some fresh tunes :)"
             className="absolute bottom-10 right-0 left-0 mx-auto w-[90%] text-center text-[14px] font-semibold uppercase text-[#e4ded7] sm:w-[500px] md:bottom-12 md:w-[550px] md:text-[16px] "
-          />
+          /> */}
         </div>
-      </div>
+      {/* </div> */}
     </section>
   );
 };
